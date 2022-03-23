429547865455c6496899f645613929a0282b9e4e
Nikolay Ogorodnikov <kolaqsq1707@gmail.com>
2022-03-23 22:23:08 +0300

    added dynamic colors

--- app/src/main/java/com/example/currencyconvertus/CurrencyConvertus.kt
+++ app/src/main/java/com/example/currencyconvertus/CurrencyConvertus.kt
@@ -3,9 +3,14 @@ package com.example.currencyconvertus
 import android.app.Application
 import com.example.currencyconvertus.data.CurrencyRepository
 import com.example.currencyconvertus.data.RemoteDataSource
+import com.google.android.material.color.DynamicColors
 
 class CurrencyConvertus : Application() {
-
     private val currencyDAO by lazy { RemoteDataSource.provideCurrencyDAO() }
     val repository by lazy { CurrencyRepository(currencyDAO) }
+
+    override fun onCreate() {
+        super.onCreate()
+        DynamicColors.applyToActivitiesIfAvailable(this)
+    }
 }
